a1bafb4219f9436a1dc44435fbfe93500e9d17a8
jialiu <jialiu@CNjialiu.local>
2018-03-17 17:43:21 +0800

    [lj]#2-59

---
+++ chapter2/homework/2-59/solution.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+int main(int argc, char *argv[])
+{
+    int32_t x = 0x89ABCDEF;
+    int32_t y = 0x76543210;
+    printf("%x \n", (x & 0x000000FF) | (y & 0xFFFFFF00));
+    return 0;
+}
